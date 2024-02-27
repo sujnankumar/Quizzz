9e5c722f8055e0ea2be9c64568d48f52ab1b0144
sujnankumar <139937713+sujnankumar@users.noreply.github.com>
2024-02-27 08:39:00 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,17 @@
+# Importing required libraries
+import os
 from app import create_app
 
+# Get the environment variables
+global_variable = os.environ.get('GLOBAL_VARIABLE')
+quizz_variable = os.environ.get('QUIZZ_VARIABLE')
+
+# Create the Flask app
 app = create_app()
+
+# Set the secret key from environment variable
 app.secret_key = 'your_secret_key'
 
 if __name__ == '__main__':
+    # Run the app
     app.run(debug=True)
